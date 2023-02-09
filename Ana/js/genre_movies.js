e4fae662c806a603e8cd6e577960a76d8128ffdf
AnaGonz2020 <thehazeplace@Anas-MBP.lan>
2023-02-08 19:54:53 -0800

    updating html file

--- Ana/js/genre_movies.js
+++ Ana/js/genre_movies.js
@@ -40,15 +40,4 @@ function closeSidebar() {
 //     }]
 //   }
 
-let trace1 = {
-    x: xData,
-    y: yData
-  };
-  
-  let data = [trace1];
-  
-  let layout = {
-    title: "A Plotly plot"
-  };
-  
-  Plotly.newPlot("plot", data, layout);
+
