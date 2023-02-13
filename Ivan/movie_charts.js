20e8b2f5b98a2e240d2176a392047e07e76d2da8
Ivan Villa <itanalytics21@gmail.com>
2023-02-12 17:49:34 -0800

    Update

--- Ivan/movie_charts.js
+++ Ivan/movie_charts.js
@@ -1,2 +1,16 @@
-
-let myTable = new JSTable("#basic");
+ 
+ fetch("./Top_748_Films.json")
+ .then(response => {
+    return response.json();
+ })
+ .then(film_data => console.log(film_data));
+ 
+import {
+    DataTable,
+    convertJSON
+  } from "/simple-datatables"
+  const dataTable = new DataTable("#myTable")
+  const convertedData = convertJSON({
+    data: film_data
+  })
+  dataTable.insert(convertedData) 
